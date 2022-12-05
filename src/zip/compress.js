9a0ec02d900159865a951bf3bc8f5510398b03f0
Dmitriy Zamulin <zamulin1998@gmail.com>
2022-12-05 18:12:26 +0300

    feat: solve wt and zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,23 @@
+import * as fs from "fs";
+import * as zlib from "zlib";
+import * as url from "url";
+
+const __dirName = url.fileURLToPath(new URL(".", import.meta.url));
+
 const compress = async () => {
-    // Write your code here 
+  const readStream = fs.createReadStream(
+    `${__dirName}/files/fileToCompress.txt`
+  );
+  const writeStream = fs.createWriteStream(`${__dirName}/files/archive.gz`);
+
+  const compressStream = zlib.createGzip();
+
+  readStream
+    .pipe(compressStream)
+    .pipe(writeStream)
+    .on("error", (err) => {
+      throw err;
+    });
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,23 @@
+import * as fs from "fs";
+import * as zlib from "zlib";
+import * as url from "url";
+
+const __dirName = url.fileURLToPath(new URL(".", import.meta.url));
+
 const decompress = async () => {
-    // Write your code here 
+  const readStream = fs.createReadStream(`${__dirName}/files/archive.gz`);
+  const writeStream = fs.createWriteStream(
+    `${__dirName}/files/fileToCompress.txt`
+  );
+
+  const decompressStream = zlib.createUnzip();
+
+  readStream
+    .pipe(decompressStream)
+    .pipe(writeStream)
+    .on("error", (err) => {
+      throw err;
+    });
 };
 
 await decompress();
