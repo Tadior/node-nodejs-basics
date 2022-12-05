3ce5e826c1621e67f6ce3527d9c78e221a2e8224
Dmitriy Zamulin <zamulin1998@gmail.com>
2022-12-05 14:21:41 +0300

    feat: solve fs tasks

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import fs from "fs/promises";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __fileName = fileURLToPath(import.meta.url);
+const __dirName = dirname(__fileName);
+const pathToFile = path.join(__dirName, "files", "fresh.txt");
+
 const create = async () => {
-    // Write your code here 
+  try {
+    await fs.access(pathToFile, fs.constants.F_OK);
+    console.error("Error FS operation failed");
+  } catch {
+    fs.writeFile(pathToFile, "I am fresh and young");
+  }
 };
 
 await create();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,18 @@
+import * as fs from "fs";
+import * as url from "url";
+
+const __dirName = url.fileURLToPath(new URL(".", import.meta.url));
+const fsPromises = fs.promises;
+
 const rename = async () => {
-    // Write your code here 
+  fsPromises
+    .rename(
+      `${__dirName}/files/wrongFilename.txt`,
+      `${__dirName}/files/properFilename.md`
+    )
+    .catch(() => {
+      throw new Error("FS operation failed");
+    });
 };
 
 await rename();
